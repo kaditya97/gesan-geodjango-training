b9fdf9869479ccf35d26fefacb57b9814cddddca
kaditya97 <kaditya9711@gmail.com>
2024-02-10 17:11:04 +0545

    feat: geojson and geojson download response for vector layer

--- data/views.py
+++ data/views.py
@@ -1,3 +1,7 @@
+import json
+from tempfile import NamedTemporaryFile
+from django.core.serializers import serialize
+from django.http import FileResponse
 from rest_framework import permissions, status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.parsers import FormParser, MultiPartParser
@@ -5,8 +9,9 @@ from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
-from data.models import VectorLayer, RasterLayer
+from data.models import VectorLayer, RasterLayer, FeatureCollection
 from data.serializers import (
     VectorLayerSerializer,
     VectorLayerPostSerializer,
@@ -72,6 +77,39 @@ class VectorViewSet(ModelViewSet):
                 instance.delete()
                 return Response("Error parsing file", status=status.HTTP_400_BAD_REQUEST)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+    @action(detail=False, methods=['get'])
+    def geojson(self, request):
+        vector_id = request.query_params.get('vector_id', None)
+        if not vector_id:
+            return Response("vector_id is required", status=status.HTTP_400_BAD_REQUEST)
+        self.queryset = FeatureCollection.objects.filter(vectorlayer=vector_id)
+        if not self.queryset:
+            return Response("vector layer not found", status=status.HTTP_404_NOT_FOUND)
+        data = serialize(
+            "geojson",
+            self.queryset,
+            geometry_field="geom",
+        )
+        data = json.loads(data)
+        return Response(data)
+    
+    @action(detail=False, methods=['get'])
+    def download(self, request):
+        vector_id = request.query_params.get('vector_id', None)
+        if not vector_id:
+            return Response("vector_id is required", status=status.HTTP_400_BAD_REQUEST)
+        self.queryset = FeatureCollection.objects.filter(vectorlayer=vector_id)
+        if not self.queryset:
+            return Response("vector layer not found", status=status.HTTP_404_NOT_FOUND)
+        data = serialize(
+            "geojson",
+            self.queryset,
+            geometry_field="geom",
+        )
+        with NamedTemporaryFile(mode='w', delete=False) as temp_file:
+            temp_file.write(data)
+        return FileResponse(open(temp_file.name, 'rb'), as_attachment=True, filename="data.geojson")
 
 class RasterViewSet(ModelViewSet):
     queryset = RasterLayer.objects.all()
